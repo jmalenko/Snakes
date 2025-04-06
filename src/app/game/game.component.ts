0af2a93edb46f33631ea066aef4e0ac5e70fed61
Jaromir Malenko <jaro@jaro.cz>
2025-04-07 00:22:33 +0200

    Control snake with keys

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, ViewChild} from '@angular/core';
+import {Project, Path, Color, Point, View} from 'paper';
 
 @Component({
   selector: 'app-game',
@@ -6,6 +7,90 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export class GameComponent {
+export class GameComponent implements AfterViewInit {
 
+  @ViewChild('canvas') canvas: ElementRef;
+
+  // TODO Map internal coordinates to real canvas size
+  // WIDTH = 1920; // pixels
+  WIDTH = 800; // pixels
+  HEIGHT = this.WIDTH * 9 / 16; // pixels
+
+  SPEED = 200; // pixels per second
+  ANGLE = 360; // turn by this angle, per second
+
+  THICKNESS = 8; // pixels
+
+  // TODO: Change FPS to realtime
+  FPS = 5; // frames per second
+
+  snake: paper.Path;
+  head: paper.Point;
+  vector: paper.Point;
+
+  pressedKeys = new Map<string, boolean>();
+
+  ngAfterViewInit() {
+    const project = new Project(this.canvas.nativeElement);
+
+    this.snake = new Path();
+    this.snake.strokeColor = Color.random();
+    this.snake.strokeWidth = this.THICKNESS;
+
+    this.head = new Point(this.WIDTH / 2, 0.5 * this.HEIGHT);
+    this.vector = new Point(this.SPEED / this.FPS, 0);
+    this.snake.add(this.head);
+
+    let delay_per_frame = 1000 / this.FPS; // ms
+    setInterval(() => {
+      this.tick();
+    }, delay_per_frame);
+  }
+
+  private tick() {
+    console.log("Tick");
+
+    let angle_per_frame = this.ANGLE / this.FPS;
+
+    if (this.pressedKeys.get("ArrowLeft"))
+      this.vector.angle -= angle_per_frame;
+    if (this.pressedKeys.get("ArrowRight"))
+      this.vector.angle += angle_per_frame;
+
+    let newHead = this.head.add(this.vector);
+
+    if (this.detectCrash(newHead)) {
+      console.log("Crash")
+      return;
+    }
+
+    this.head = newHead;
+
+    this.snake.add(this.head);
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyDownEvent(event: KeyboardEvent) {
+    console.log("Key down " + event.key + " " + event.code + " " + event.keyCode);
+    this.pressedKeys.set(event.code, true);
+  }
+
+  @HostListener('document:keyup', ['$event'])
+  handleKeyUpEvent(event: KeyboardEvent) {
+    console.log("Key up " + event.key + " " + event.code);
+    this.pressedKeys.set(event.code, false);
+  }
+
+  detectCrash(newHead: paper.Point) {
+    for (const segment of this.snake.segments) {
+      const point = segment.point
+      const distance = newHead.getDistance(point)
+      if (distance < 10) {
+        console.log("New head " + newHead);
+        console.log("Crash point " + point);
+        return true;
+      }
+    }
+    return false;
+  }
 }
